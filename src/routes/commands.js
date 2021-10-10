0575a52812886a2ef49c03d1f9d70e44f91add8f
hotbear1110 <mads095h@gmail.com>
2021-10-10 20:18:06 +0200

    Made the site a bit cleaner (Still work in progress)

--- src/routes/commands.js
+++ src/routes/commands.js
@@ -1,10 +1,23 @@
 const tools = require('../../public/js/tools.js');
 const express = require('express');
+const { forEach } = require('underscore');
 const commandRouter = express.Router();
 
 commandRouter.get('', async(req, res) => {
     const commandlist = await tools.query(`SELECT * FROM Commands`);
-    res.render('commands', { commands : commandlist })
+
+    let categorylist = [];
+
+    commandlist.forEach(function(command) {
+
+        if (!categorylist.includes(command.Category)) {
+            categorylist.push(command.Category);
+        }
+    })
+    categorylist = [categorylist[2], categorylist[3], categorylist[1], categorylist[0], categorylist[4]]
+
+    console.log(categorylist)
+    res.render('commands', { commands : commandlist , categories : categorylist})
 })
 
 module.exports = commandRouter;
